5b05b8d87329c4f7bfbe3ce8e1492ea034ac6d5e
Fernando Andrade <fernandofreamunde@gmail.com>
2017-12-31 00:14:28 +0100

    invoice repo added

---
+++ Src/AppPacket/Repository/InvoiceRepository.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace App\AppPacket\Repository;
+
+use App\Core\Query;
+use App\Core\Database;
+use App\AppPacket\Entity\Invoice;
+
+/**
+* 
+*/
+class InvoiceRepository
+{
+    const ITEMS_PER_PAGE = 5;
+    protected $entity;
+    private $db;
+
+    public function __construct(Invoice $invoice, Database $db)
+    {
+        $this->entity = $invoice;
+        $this->db = $db;
+    }
+
+    public function getAllPaginated(int $page = 1)
+    {
+        $page = $page < 1 ? 1 : $page;
+
+        $offset = ($page -1) * self::ITEMS_PER_PAGE;
+
+        $query = $this->getQuery();
+        $query->select();
+        $query->offsetAndLimit($offset, self::ITEMS_PER_PAGE);
+
+        return $this->db->runQuery($query);
+    }
+
+    public function getAll()
+    {
+        $query = $this->getQuery();
+        $query->select();
+
+        return $this->db->runQuery($query);
+    }
+
+    private function getQuery()
+    {
+        return new Query($this->entity);
+    }
+}
